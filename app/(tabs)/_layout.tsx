bae1766cd4d966df4f6de18c94b6e3b85977e3ca
EmekaAgara <emekaagara@gmail.com>
2025-01-17 11:16:16 +0100

    env xhange

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,43 +1,75 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+import { HapticTab } from "@/components/HapticTab";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import FontAwesome6 from "@expo/vector-icons/FontAwesome6";
+import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
+import Ionicons from "@expo/vector-icons/Ionicons";
+import MaterialIcons from "@expo/vector-icons/MaterialIcons";
 
 export default function TabLayout() {
-  const colorScheme = useColorScheme();
-
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
+        tabBarActiveTintColor: Colors.PRIMARY,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
-          },
-          default: {},
-        }),
-      }}>
+        // tabBarStyle: Platform.select({
+        //   ios: {
+        //     // Use a transparent background on iOS to show the blur effect
+        //     position: "absolute",
+        //   },
+        //   default: {},
+        // }),
+      }}
+    >
       <Tabs.Screen
-        name="index"
+        name="home"
         options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
+          tabBarLabel: "Home",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome6 name="house" size={20} color={color} />
+          ),
         }}
       />
       <Tabs.Screen
-        name="explore"
+        name="services"
         options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+          tabBarLabel: "Services",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome5 name="tools" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="post"
+        options={{
+          tabBarLabel: "Post",
+
+          tabBarIcon: ({ color }) => (
+            <Ionicons name="add-circle" size={26} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="marketplace"
+        options={{
+          tabBarLabel: "Marketplace",
+          tabBarIcon: ({ color }) => (
+            <FontAwesome6 name="shop" size={20} color={color} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="account"
+        options={{
+          tabBarLabel: "Account",
+          tabBarIcon: ({ color }) => (
+            <MaterialIcons name="account-circle" size={20} color={color} />
+          ),
         }}
       />
     </Tabs>
---
+++ app/(tabs)/account.jsx
@@ -0,0 +1,50 @@
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
+import React from "react";
+import { useAuth } from "@clerk/clerk-expo";
+import { useRouter } from "expo-router";
+
+export default function account() {
+  const router = useRouter();
+  const { signOut } = useAuth();
+
+  const logout = () => {
+    signOut();
+    router.push("GetStarted"); // Navigate to the tabs screen
+    Alert.alert("Logged Out", "You can login later!");
+  };
+
+  return (
+    <View>
+      <Text>account</Text>
+      <TouchableOpacity onPress={logout} style={styles.logoutButton}>
+        <Text style={styles.logoutText}>Logout</Text>
+      </TouchableOpacity>
+
+      <View
+        style={{
+          padding: 20,
+        }}
+      >
+        <Text
+          style={{
+            fontFamily: "outfit-bold",
+            fontSize: 35,
+          }}
+        >
+          {" "}
+          Profile
+        </Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  logoutButton: {
+    padding: 10,
+    backgroundColor: "red",
+    borderRadius: 5,
+    marginTop: 20,
+  },
+  logoutText: { color: "white", fontWeight: "bold" },
+});
